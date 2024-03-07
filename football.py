5fd1084c21fd3908cbe421604a25e156019239bf
n1k61n <eminelxanoglu@gmail.com>
2024-03-07 10:51:06 +0400

    make change

--- football.py
+++ football.py
@@ -1,27 +1,30 @@
 from random import choice
 
+def calc_stats(iter_times:int, money:int):
+    coefficients = [1.50, 1.60, 1.70, 1.80, 1.90]
+    bank = money
 
-def analize_football(bank:float, times:int)->float:
-    res = 0.0
-    coef = [1.50, 1.60, 1.70, 1.80, 1.90]
-    while times > 0 and bank > 0:
-        if choice((True,True, False)):
-            bank = (bank / 2) + ((bank / 2) * choice(coef))
+    for i in range(iter_times):
+        chanse = choice((True, True,  False))
+        if chanse:
+            bank = (bank / 2) +  ((bank / 2) * chanse)
         else:
-            bank -= (bank / 2)
-        times -= 1
+            bank -= bank / 2
+        if bank < 1:
+            print("You lose your money!!")
+            break
+
     return bank
-        
 
 
 
 
 
 def main():
-    money = int(input("Enter now many money do you spent: "))
-    times = int(input("How much times do it: "))
-    res = analize_football(money, times)
-    print(f"You earn money: {res:.2f}")
+    money = int(input("Enter how many money do you spent: "))
+    iteration = int(input("How many times: "))
+    result = calc_stats(iteration, money)
+    print(f"You earn money {result:.2f}") 
 
 
 
